c4d79eff17d66e484f1636007deb5cae45353f50
halyson lima <halyson.lima@hotmail.com>
2021-08-21 13:26:46 -0300

    mudou as variaveis

--- basico/Variaveis.java
+++ basico/Variaveis.java
@@ -5,6 +5,26 @@ public class Variaveis {
 	public static void main(String[] args) {
 		System.out.println("   /)_/)\n  (=';')\n c(('')('')\n______________");
 
+		
+		int Num1,Num2, Soma, Subtracao, Divisao, multiplicacao ;
+		Num1 = 25;
+		Num2 = 5;		
+		
+		
+	 Soma =	Num1 + Num2;
+	 Subtracao = Num1 - Num2;
+	 Divisao = Num1/Num2;
+	 multiplicacao = Num1*Num2;
+	 
+	 System.out.println(Soma);
+	 System.out.println(Subtracao);
+	 System.out.println(Divisao);
+	 System.out.println(multiplicacao);
+
+		
+		
+		
+		
 	}
 
 }
